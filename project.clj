1080ad90a31890bd068a8b9ccecc8c5d0d4196e3
Chet Mancini <chetmancini@gmail.com>
2015-03-18 09:56:54 -0400

    CM: Add utils file

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject blazing "0.1.0-SNAPSHOT"
+  :description "Clojure library for Spark Streaming with Flambo"
+  :url "http://chetmancini.com/blazing"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"])
---
+++ test/blazing/utils_test.clj
@@ -0,0 +1,1 @@
+(ns blazing.utils-test)
