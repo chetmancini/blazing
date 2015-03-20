bb902cf03edeb99daf901ff759253e77374d8db8
Chet Mancini <chetmancini@gmail.com>
2015-03-20 19:34:36 -0400

    CM: spike out wordcount example

---
+++ src/blazing/wordcount.clj
@@ -0,0 +1,31 @@
+(ns blazing.wordcount
+  (:require [flambo.conf :as conf]
+            [flambo.api :as f]
+            [flambo.streaming :as fs])
+  (:require [clojure.string :as s])
+  (:gen-class))
+
+(def master "local")
+(def app-name "blazing")
+(def conf {})
+(def env {
+           "spark.executor.memory" "1G",
+           "spark.files.overwrite" "true"})
+
+(defn -main [& args]
+  (let [c (-> (conf/spark-conf)
+              (conf/master master)
+              (conf/app-name "adapters")
+              (conf/set "spark.akka.timeout" "300")
+              (conf/set-executor-env env))
+        ssc (fs/streaming-context c 2000) ;; get a streaming context with a 2 second batch interval
+        stream (fs/text-file-stream "filename")]
+
+    (-> stream
+        (fs/map (memfn _2))
+        (fs/flat-map (f/fn [l] (s/split l #" ")))
+        (fs/map (f/fn [w] [w 1]))
+        (fs/reduce-by-key-and-window (f/fn [x y] (+ x y)) (* 10 60 1000) 2000)
+        (fs/print))
+    (.start ssc)
+    (.awaitTermination ssc)))
